5947c0b38827cd59228e2e12b03e2a1315c26919
Rex-David Buchi <107920648+RexDavid06@users.noreply.github.com>
2025-01-31 07:41:19 +0100

    database modification

--- stage0/settings.py
+++ stage0/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'stage0.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("MARIADB_DATABASE"),
-        "USER": os.getenv("MARIADB_USER"),
-        "PASSWORD": os.getenv("MARIADB_ROOT_PASSWORD"),
-        "HOST": os.getenv("MARIADB_HOST"),
-        "PORT": os.getenv("MARIADB_PORT", 3306),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
